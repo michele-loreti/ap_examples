8b24874e4b9166d526550c5607771ce916ab3a89
Michele Loreti <loreti@Micheles-MBP.homenet.telecomitalia.it>
2018-03-11 10:08:24 +0100

    Fixed names

--- gcd/gcd/Program.fs
+++ gcd/gcd/Program.fs
@@ -6,15 +6,15 @@ let readUInt msg =
     printf msg;
     Console.ReadLine () |> uint32
 
-let mcd a b = 
-    let rec _mcd x y = 
+let gcd a b = 
+    let rec _gcd x y = 
         if y=0u then x
-        else _mcd y (x%y) 
-    _mcd (max a b) (min a b)
+        else _gcd y (x%y) 
+    _gcd (max a b) (min a b)
 
 [<EntryPoint>]
 let main argv =
     let v1 = readUInt "Insert v1: "
     let v2 = readUInt "Insert v2: "
-    printfn "mcd %d %d = %d" v1 v2 (mcd v1 v2)
+    printfn "mcd %d %d = %d" v1 v2 (gcd v1 v2)
     0 // return an integer exit code
